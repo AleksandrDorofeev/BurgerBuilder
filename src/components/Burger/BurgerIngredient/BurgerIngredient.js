6ce740fbd7ccfc3ec4f47607bd9f7eb590f6c32a
Dorofeev Aleksandr <dorofeevaleksandr@Dorofeev-Aleksandr.local>
2018-06-08 17:53:27 +0300

    4

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import classes from './BurgerIngregient.css';
 
 const BurgerIngregient = (props) => {
+    let ingredient = null;
 
+    switch(props.type) {
+        case('bread-bottom'):
+        ingredient = <div className={classes.Breadbottom}></div>
+    }
 }
 
 export default BurgerIngregient;
